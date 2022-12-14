241309e56d6789b03133ed338b7db6b8d77c7e34
TepNik <tepelin.nikita@gmail.com>
2022-12-15 02:28:59 +0600

    Wrapped into the function

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,53 @@
-import { Configuration, OpenAIApi } from "openai";
+import { Configuration, OpenAIApi } from 'openai';
 
-import { config } from "../config.js";
+import { config } from '../config.js';
 
 const configuration = new Configuration({
-  apiKey: config.openAiApi,
+    apiKey: config.openAiApi,
 });
 const openai = new OpenAIApi(configuration);
 
-const textResponse = await openai.createCompletion({
-    model: "text-davinci-001",
-    prompt: "Write description of a picture on the theme forest.\n",
-    temperature: 0.4,
-    max_tokens: 240,
-    top_p: 1,
-    frequency_penalty: 0,
-    presence_penalty: 0,
-    n: 10
-});
+interface ImageGenerationResponse {
+    text: string;
+    imageUrl?: string;
+    b64_json?: string;
+}
 
-for(let i = 0; i < textResponse.data.choices.length; ++i) {
-    const text = textResponse.data.choices[i].text;
-    const imageResponse = await openai.createImage({
-        prompt: text,
+async function getImagesFromText(
+    initialPrompt: string,
+    n: number,
+): Promise<ImageGenerationResponse[]> {
+    const textResponse = await openai.createCompletion({
+        model: 'text-davinci-001',
+        prompt: initialPrompt,
+        temperature: 0.4,
+        max_tokens: 240,
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0,
+        n: n,
     });
 
-    console.log("Text:",text, "\nImage: ", imageResponse.data.data[0].url, "\n");
+    const result: ImageGenerationResponse[] = [];
+    for (let i = 0; i < textResponse.data.choices.length; ++i) {
+        const text = textResponse.data.choices[i].text.replace('\n', '');
+        const imageResponse = await openai.createImage({
+            prompt: text,
+        });
+
+        result.push({
+            text,
+            imageUrl: imageResponse.data.data[0].url,
+            b64_json: imageResponse.data.data[0].b64_json,
+        });
+    }
+
+    return result;
 }
+
+console.log(
+    await getImagesFromText(
+        'Write description of a picture on the theme forest.',
+        1,
+    ),
+);
